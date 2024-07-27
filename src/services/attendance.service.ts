e45c0eb20d81aad359ab3472063d8af946e1f7aa
Youssefwael397 <youssefwael397@gmail.com>
2024-07-27 20:57:56 +0300

    refactor attendance

--- src/services/attendance.service.ts
+++ src/services/attendance.service.ts
@@ -8,11 +8,30 @@ import { NotFoundError } from '../errors/NotFoundError';
 import { BadRequestError } from '../errors/BadRequestError';
 import { plainToInstance } from 'class-transformer';
 import { UpdateTraineeDTO } from '../dtos/trainee.dto';
+import { ConflictError } from '../errors/ConflictError';
 
 class AttendanceService {
   async createAttendance(parcode: string) {
     const trainee = await traineeService.getTraineeByParcode(parcode);
+
     if (!trainee) throw new NotFoundError('Could not find trainee');
+
+    const today = new Date();
+    const startOfDay = new Date(today.setHours(0, 0, 0, 0));
+    const endOfDay = new Date(today.setHours(23, 59, 59, 999));
+
+    const duplicateAttendance = await prisma.attendance.findFirst({
+      where: {
+        traineeId: trainee.id,
+        date: {
+          gte: startOfDay,
+          lte: endOfDay,
+        },
+      },
+    });
+
+    if (duplicateAttendance)
+      throw new ConflictError('This trainee has already attended');
 
     if (trainee.remainingClasses === 0)
       throw new BadRequestError({
@@ -30,17 +49,43 @@ class AttendanceService {
 
     return prisma.attendance.create({
       data: { traineeId: trainee.id },
-    });
-  }
-
-  async getAllAttendances(): Promise<Attendance[]> {
-    return prisma.attendance.findMany({
       include: {
         trainee: {
           select: TraineeWithoutPassword,
         },
       },
     });
+  }
+
+  async getAllAttendances(): Promise<any> {
+    const attendances = await prisma.attendance.findMany({
+      include: {
+        trainee: {
+          select: TraineeWithoutPassword,
+        },
+      },
+    });
+
+    // Group attendances by day
+    const groupedAttendances = this.groupAttendancesByDay(attendances);
+
+    return groupedAttendances;
+  }
+
+  private groupAttendancesByDay(attendances: Attendance[]): any {
+    const grouped = attendances.reduce((acc, attendance) => {
+      const date = new Date(attendance.date);
+      const day = date.toISOString().split('T')[0]; // Get YYYY-MM-DD format
+
+      if (!acc[day]) {
+        acc[day] = [];
+      }
+
+      acc[day].push(attendance);
+      return acc;
+    }, {} as { [key: string]: Attendance[] });
+
+    return grouped;
   }
 
   async getAttendanceByTraineeId(traineeId: number): Promise<Attendance[]> {
