5d2bb3e68037171c0a131189372eb564e4fd2790
Nouran-Ali <noranali237@gmail.com>
2024-08-30 21:54:25 +0300

    edit

--- src/index.ts
+++ src/index.ts
@@ -36,6 +36,6 @@ app.use(errorHandler);
 //   console.log(`Server is running on http://${host}:${port}`);
 // });
 
-app.listen(port , host , () => {
+app.listen(process.env.PORT , () => {
   console.log(`Server is running on http://${host}:${port}`);
 });
